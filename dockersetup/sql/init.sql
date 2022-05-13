0306ace5dad68a7ffdb0bd7a90b97725a1d0a09a
śrīnivāsa kaśyap munukutla <skmnktl@gmail.com>
2022-05-13 11:43:20 -0500

    user testing

--- dockersetup/sql/init.sql
+++ dockersetup/sql/init.sql
@@ -1,7 +1,7 @@
 USE db;
 
 CREATE TABLE user (
-	id INT unsigned NOT NULL AUTO_INCREMENT,
+	userid INT unsigned NOT NULL AUTO_INCREMENT,
 	password TEXT(256),
 	email TEXT(256),
 	accountType TEXT(3),
@@ -10,35 +10,35 @@ CREATE TABLE user (
 	lastname TEXT(256),
     active BOOLEAN,
     username TEXT(256),
-    PRIMARY KEY (id)
+    PRIMARY KEY (userid)
 );
 
 CREATE TABLE `course` (
-	id INT unsigned NOT NULL AUTO_INCREMENT,
+	courseid INT unsigned NOT NULL AUTO_INCREMENT,
 	coursename TEXT(256),
 	coursedescription VARCHAR(2000),
 	coursecapacity INT,
 	professor TEXT(256),
-	students VARCHAR(2000),
+	student VARCHAR(2000),
 	announcementInbox VARCHAR(2000),
-    PRIMARY KEY (id)
+    PRIMARY KEY (courseid,student)
 
 );
 
 CREATE TABLE assignment (
-	id INT unsigned NOT NULL,
+	assignmentid INT unsigned NOT NULL,
     name TEXT(256),
     description VARCHAR(2000),
     points INT,
     duedate DATE,
     courseid INT unsigned,
     student INT unsigned,
-    PRIMARY KEY (courseid, id, student)
+    PRIMARY KEY (courseid, assignmentid, student)
 );
 
 CREATE TABLE announcements (
     message VARCHAR(2000),
     senddate DATE,
-    course INT
+    courseid INT
 );
 
