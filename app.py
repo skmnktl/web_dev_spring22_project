ee9171af621e504bf5498bcd770408547532ea50
śrīnivāsa kaśyap munukutla <skmnktl@gmail.com>
2022-05-20 22:47:37 -0500

    assignment refactoring

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def createAccount():
     securityAnswer2 = ""
     securityAnswer3 = ""
 
-    print(firstName)
+    print(f"firstName is {firstName}")
 
     if form.validate_on_submit():
         print("CREATING NEW ACCOUNT")
