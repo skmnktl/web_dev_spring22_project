d5deae984be06024f3f086742342cb02c4e61b79
śrīnivāsa kaśyap munukutla <skmnktl@gmail.com>
2022-05-21 00:27:39 -0500

    assignment refactoring

--- forms.py
+++ forms.py
@@ -8,10 +8,11 @@ class AccountForm(FlaskForm):
 	email          = StringField("Email: ",       validators=[DataRequired()])
 	accountID      = IntegerField("Account ID: ", validators=[DataRequired()])
 	password       = StringField("Password: ",    validators=[DataRequired()])
-	accountType    = RadioField("Account Type",   choices=[("student","Student"),("teacher","Teacher")])
-	securityAnswer1 = StringField("Your Answer: ", validators=[DataRequired()])
-	securityAnswer2 = StringField("Your Answer: ", validators=[DataRequired()])
-	securityAnswer3 = StringField("Your Answer: ", validators=[DataRequired()])
+	accountType    = RadioField("Account Type",   choices=[("student",
+                                                            "Student"),
+                                                           ("teacher",
+                                                            "Teacher"),
+                                                           ("admin","Admin")])
 	submit = SubmitField("Create Account")
 
 
