33818dba35162f74274f1b0f6e0e5377cc9be73e
śrīnivāsa kaśyap munukutla <skmnktl@gmail.com>
2022-05-26 14:38:19 -0500

    assignment api req fix

--- app.py
+++ app.py
@@ -449,7 +449,7 @@ def addToCourse():
         print(course.values())
     if form.validate_on_submit():
         coursedata = request.form['rowCourseID']
-        courseid = list(coursedata)
+        courseid = json.loads(coursedata)
         print(courseid)
         requests.post(backend+"/addstudenttocourse",params={"courseid":courseid,"userid":userid})
     # edit db
