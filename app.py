fe12b79a5e491a93514c71ec4ec4f0304f502194
śrīnivāsa kaśyap munukutla <skmnktl@gmail.com>
2022-05-26 19:20:26 -0500

    assignment api req fix

--- app.py
+++ app.py
@@ -467,9 +467,14 @@ def addToCourse():
 def myAccount():
     form = MyAccountForm()
     userid = request.args
-    print(userid)
+    user = requests.get(backend+"/getuser",params={"userid":userid})
     return render_template(
-                                "myAccount.html"
+                                "myAccount.html",
+                                userData = userdata
+                                firstname = user['firstName'],
+                                lastname = user['firstname'],
+                                email = user['email'],
+                                headings = ["User ID","First Name", "Last Name", "Email"]
                             )
 # Internal Server Error
 @app.errorhandler(500)
